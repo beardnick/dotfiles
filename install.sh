24dd061798758b304f058da6e485d03ec4bd5c22
qianz <1685437606@qq.com>
2019-08-20 11:41:01 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# ideavim
+stow -t ~/               idea
+
+# ranger
+stow -t ~/.config/ranger ranger
+
+# .zshrc .zsh_history
+stow -t ~/               shell
+
+# init.toml myspacevim.vim
+stow -t ~/.SpaceVim.d    spacevim
+
+# .tmux.conf
+stow -t ~/               tmux
