49bf1afb3f46d07b8793757f02ee43bbeec99f4d
qianz <1685437606@qq.com>
2022-03-05 13:41:56 +0800

    feat:add user rsa pub key and execute init.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,12 @@ mkdir -p /root/.ssh
 # .ssh files need be copyed instead of soft link
 if [[ -e /data/.ssh ]]; then
     cp -v /data/.ssh/* /root/.ssh
+    cat /data/.ssh/id_rsa.pub >>  /root/.ssh/authorized_keys
 fi
 
+if [[ -f /data/init.sh ]]; then
+    zsh init.sh
+fi
+
+# prevent container from existing
 tail -f /dev/null
