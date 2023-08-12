d3b96d75d544c96e59362238bcce57df7daca026
qianz <1685437606@qq.com>
2023-08-12 09:41:22 +0800

    feat:disable char mode in flash

--- vim/my.nvim/plugin/flash.lua
+++ vim/my.nvim/plugin/flash.lua
@@ -2,13 +2,21 @@ if not require('plug').loaded('flash.nvim') then
     return
 end
 
-local map =vim.api.nvim_set_keymap
-local mapopt = {noremap = true, silent = true}
+local map = vim.api.nvim_set_keymap
+local mapopt = { noremap = true, silent = true }
 
-require('flash').setup()
+require('flash').setup(
+    {
+        modes = {
+            char = {
+                enabled = false
+            }
+        }
+    }
+)
 
-map('n','s',[[<cmd>lua require('flash').jump()<cr>]],mapopt)
+map('n', 's', [[<cmd>lua require('flash').jump()<cr>]], mapopt)
 
-vim.api.nvim_set_hl(0, "FlashLabel", { ctermbg=0, bg='#fa036a' })
-vim.api.nvim_set_hl(0, "FlashMatch", { ctermbg=0, bg='#2f50cd' })
-vim.api.nvim_set_hl(0, "FlashCurrent", { ctermbg=0, bg='#2f50cd' })
+vim.api.nvim_set_hl(0, "FlashLabel", { ctermbg = 0, bg = '#fa036a' })
+vim.api.nvim_set_hl(0, "FlashMatch", { ctermbg = 0, bg = '#2f50cd' })
+vim.api.nvim_set_hl(0, "FlashCurrent", { ctermbg = 0, bg = '#2f50cd' })
