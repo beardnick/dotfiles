b699c3972a58a5ed54c1ac47c1abfcbcace31fb6
qianz <qianzhoubeard@gmail.com>
2025-01-29 14:53:39 +0800

    feat:update bootstrap.sh

---
+++ bootstrap.mjs
@@ -0,0 +1,58 @@
+const path = require('path');
+const fs = require('fs');
+
+function ensureDir(dir) {
+  if (fs.existsSync(dir)) {
+    return;
+  }
+  fs.mkdirSync(dir, {recursive: true});
+}
+
+async function ensureAllLink(src, dst) {
+  const files = await glob(`${src}/*`);
+  for (const f of files) {
+    ensureLink(f, `${dst}/${path.basename(f)}`)
+  }
+}
+
+function ensureLink(src, dst) {
+  if (fs.existsSync(dst)) {
+    const stats = fs.lstatSync(dst);
+    if (stats.isSymbolicLink()) {
+      return;
+    }
+    throw new Error(`${dst} exists and not symbol link`);
+  }
+  fs.symlinkSync(src, dst);
+}
+
+const conf = `${process.env.HOME}/.config`;
+const localBin = `${process.env.HOME}/.local/bin`;
+const dotdir = (await $`pwd`).stdout.trimEnd();
+
+// ideavim
+ensureLink(`${dotdir}/idea/.ideavimrc`, `${process.env.HOME}/.ideavimrc`);
+
+// .zshrc .zsh_history
+ensureLink(`${dotdir}/shell/zsh`, `${conf}/zsh`);
+ensureLink(`${dotdir}/shell/.zshrc`, `${process.env.HOME}/.zshrc`);
+
+// .tmux.conf
+ensureLink(`${dotdir}/tmux/.tmux.conf`, `${process.env.HOME}/.tmux.conf`);
+ensureLink(`${dotdir}/tmux/.tmux.conf.local`, `${process.env.HOME}/.tmux.conf.local`);
+
+// vim
+ensureLink(`${dotdir}/vim/.vimlite.vim`, `${process.env.HOME}/.vimlite.vim`);
+ensureLink(`${dotdir}/vim/my.nvim`, `${process.env.HOME}/.config/nvim`);
+
+// vifm
+ensureLink(`${dotdir}/vifm`, `${process.env.HOME}/.config/vifm`);
+
+// navi
+
+ensureLink(`${dotdir}/navi`, `${process.env.HOME}/.config/navi`);
+
+ensureDir(`$LOCAL_BIN`);
+
+// tiny scripts
+ensureAllLink(`${dotdir}/bin`, localBin);
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,65 +1,32 @@
+# vim: set expandtab shiftwidth=4 foldmethod=marker foldlevel=0 :
 #!/usr/bin/env bash
 
+# {{{ libs
 
-CONF="$HOME/.config"
-LOCAL_BIN="$HOME/.local/bin"
-
-function ensure_dir() {
-    local path="${1:?'path is required'}"
-    if [[ -d "$path" ]]; then
-        return;
-    fi
-    mkdir -p "$path"
+function setup_ubuntu(){
+    local gui_mode="${1:-false}"
+    sudo apt update -y
+    sudo apt install -y \
+        nodejs \
+        zx
 }
 
-function link_once() {
-    local src="${1:?'src path is required'}"
-    local dst="${2:?'dst path is required'}"
-    echo "$src => $dst"
-    if [[ -e "$dst" ]]; then
-        if [[ -L "$dst" ]]; then
-            return 0;
-        fi
-        echo "$dst exists,link failed"
-        return 1;
-    fi
-    ln -s "$src" "$dst"
-}
+# }}}
 
-function link_all() {
-    local src="${1:?'link all src path is required'}"
-    local dst="${2:?'link all dst path is required'}"
-    for f in $(find "$src"/*) ; do
-        link_once "$f" "$dst/$(basename $f)"
-    done
-}
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DISTRIBUTION="$ID"
+    DISTRIBUTION_VERSION="$VERSION_ID"
+else
+    echo "unknown distribution"
+fi
 
-ensure_dir "$CONF"
+case "$DISTRIBUTION" in 
+    "ubuntu")
+        setup_ubuntu
+        ;;
+    *)
+        echo "unknonw func"
+esac
 
-DOTDIR="$(pwd)"
-# ideavim
-link_once "$DOTDIR/idea/.ideavimrc" "$HOME/.ideavimrc"
-
-# .zshrc .zsh_history
-link_once  "$DOTDIR/shell/zsh" "$CONF/zsh"
-link_once  "$DOTDIR/shell/.zshrc" "$HOME/.zshrc"
-
-# .tmux.conf
-link_once  "$DOTDIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
-link_once  "$DOTDIR/tmux/.tmux.conf.local" "$HOME/.tmux.conf.local"
-
-# vim
-link_once "$DOTDIR/vim/.vimlite.vim" "$HOME/.vimlite.vim"
-link_once "$DOTDIR/vim/my.nvim" "$HOME/.config/nvim"
-
-# vifm
-link_once "$DOTDIR/vifm" "$HOME/.config/vifm"
-
-# navi
-
-link_once "$DOTDIR/navi" "$HOME/.config/navi"
-
-ensure_dir "$LOCAL_BIN"
-
-# tiny scripts
-link_all "$DOTDIR/bin" "$LOCAL_BIN"
+zx bootstrap.mjs "$@"
