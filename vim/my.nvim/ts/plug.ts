bc3d2eb8e7d7ec3912a4b474ff4d87ec81657ae2
qianz <qianzhoubeard@gmail.com>
2025-10-03 17:00:08 +0800

    refactor: delete ts tool chains

--- vim/my.nvim/ts/plug.ts
+++
@@ -1,3 +0,0 @@
-export function loaded(p: string): boolean {
-  return vim.api.nvim_list_runtime_paths().indexOf(`${vim.g.pluginDir}/${p}`) !== -1;
-}
--- vim/my.nvim/ts/plugins/bookmarks.ts
+++
@@ -1,9 +0,0 @@
-import * as plug from '../plug'
-
-if (plug.loaded('vim-bookmarks')) {
-  vim.g.bookmark_save_per_working_dir = 1;
-  vim.g.bookmark_save_relative_dir = 1;
-  vim.fn['bm#setup']();
-}
-
-
